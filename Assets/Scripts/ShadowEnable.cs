d94c9f36c77b56a9ec38334b569fbaa05bfa9ba6
Voinova <anya_voinova@mail.ru>
2020-12-06 14:48:07 +0300

    06.12.2020
    Добавила тени

---
+++ Assets/Scripts/ShadowEnable.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowEnable : MonoBehaviour
+{
+    public SpriteRenderer spriteRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend.receiveShadows = true;
+        spriteRend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
+    }
+}
