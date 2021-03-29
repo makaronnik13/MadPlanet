8e62baa08772261ecdb4eb2064ab0f0ce5d1dffc
Voinova <anya_voinova@mail.ru>
2021-03-29 03:02:16 +0300

    Мини-правки
    Удалила пару спрайтов, немного исправила текстуру песка

---
+++ Assets/ShadowOff.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShadowOff : MonoBehaviour
+{
+    public SpriteRenderer spriteRend;
+    // Start is called before the first frame update
+    void Start()
+    {
+        spriteRend.receiveShadows = false;
+      // spriteRend.shadowCastingMode = UnityEngine.Rendering.ShadowCastingMode.TwoSided;
+    }
+}
